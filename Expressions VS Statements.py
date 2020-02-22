3218419c8da46c9edbbd28a1595425fceaa2cc3f
MorrowG2 <55847249+MorrowG2@users.noreply.github.com>
2020-02-21 17:52:52 -0800

    Add files via upload

---
+++ Expressions VS Statements.py
@@ -0,0 +1,11 @@
+# Expressions VS Statements
+# Expression produces a value
+# A statement is an entire line of code that performs an action
+
+iq = 100
+user_iq = 100 / 4
+
+def hello_world(parm1):
+    return parm1
+
+print(hello_world(1))
